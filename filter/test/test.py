3c523ab1fc53e6e6234881b098d7a568ad2b9e45
Mark Sprevak <sprevak.home@gmail.com>
2015-05-15 11:55:19 +0100

    update

--- filter/test/test.py
+++ filter/test/test.py
@@ -10,10 +10,7 @@ import panzertools
 def main():
     """docstring for main"""
     ast = json.load(sys.stdin)
-    outgoing = { 'level': 'WARNING', 'message': 'hello world' }
-    outgoing_json = json.dumps(outgoing) + '\n'
-    outgoing_bytes = outgoing_json.encode('utf8')
-    outgoing = { 'level': 'WARNING', 'message': ast }
+    outgoing = { 'level': 'INFO', 'message': json.dumps(ast) }
     outgoing_json = json.dumps(outgoing) + '\n'
     outgoing_bytes = outgoing_json.encode('utf8')
     sys.stderr.write(outgoing_bytes)
