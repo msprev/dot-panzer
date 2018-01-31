4f1c0c982cf2caab0fbd17e70b913c834218dc56
Mark Sprevak <sprevak.home@gmail.com>
2018-01-31 21:50:47 +0000

    fix https://github.com/msprev/dot-panzer/issues/7

--- filter/paginatesects/paginatesects.py
+++ filter/paginatesects/paginatesects.py
@@ -19,11 +19,10 @@ def rehead(key, value, format, meta):
     if key == 'Header' and value[0] == 1:
         if format == 'latex':
             text = '\\newpage\n\\thispagestyle{empty}\n\\setcounter{page}{1}'
-            insert = RawInline('latex', text)
+            return [RawInline('latex', text), Header(value[0], value[1], value[2])]
         elif format == 'html':
             text = '<hr>'
-            insert = RawInline('html', text)
-        return [Para([insert]), Header(value[0], value[1], value[2])]
+            return [RawInline('html', text), Header(value[0], value[1], value[2])]
 
 if __name__ == "__main__":
     panzertools.log('INFO', 'adding page break before each H1 section')
